8a950e6c3735022683a1e855eef61e17ab81eabe
Eamonn Cottrell <eamonncottrell@gmail.com>
2022-08-11 16:37:40 -0400

    get correct dump file for solution

--- periodic_table/periodic_table.sql
+++ periodic_table/periodic_table.sql
@@ -62,8 +62,7 @@ ALTER TABLE public.elements OWNER TO freecodecamp;
 
 CREATE TABLE public.properties (
     atomic_number integer NOT NULL,
-    type character varying(30),
-    atomic_mass numeric(9,6) NOT NULL,
+    atomic_mass double precision NOT NULL,
     melting_point_celsius numeric NOT NULL,
     boiling_point_celsius numeric NOT NULL,
     type_id integer NOT NULL
@@ -96,22 +95,24 @@ INSERT INTO public.elements VALUES (7, 'N', 'Nitrogen');
 INSERT INTO public.elements VALUES (8, 'O', 'Oxygen');
 INSERT INTO public.elements VALUES (2, 'He', 'Helium');
 INSERT INTO public.elements VALUES (3, 'Li', 'Lithium');
-INSERT INTO public.elements VALUES (1000, 'MT', 'moTanium');
+INSERT INTO public.elements VALUES (9, 'F', 'Fluorine');
+INSERT INTO public.elements VALUES (10, 'Ne', 'Neon');
 
 
 --
 -- Data for Name: properties; Type: TABLE DATA; Schema: public; Owner: freecodecamp
 --
 
-INSERT INTO public.properties VALUES (3, 'metal', 6.940000, 180.54, 1342, 1);
-INSERT INTO public.properties VALUES (4, 'metal', 9.012200, 1287, 2470, 1);
-INSERT INTO public.properties VALUES (1, 'nonmetal', 1.008000, -259.1, -252.9, 2);
-INSERT INTO public.properties VALUES (2, 'nonmetal', 4.002600, -272.2, -269, 2);
-INSERT INTO public.properties VALUES (6, 'nonmetal', 12.011000, 3550, 4027, 2);
-INSERT INTO public.properties VALUES (7, 'nonmetal', 14.007000, -210.1, -195.8, 2);
-INSERT INTO public.properties VALUES (8, 'nonmetal', 15.999000, -218, -183, 2);
-INSERT INTO public.properties VALUES (5, 'metalloid', 10.810000, 2075, 4000, 3);
-INSERT INTO public.properties VALUES (1000, 'metalloid', 1.000000, 10, 100, 3);
+INSERT INTO public.properties VALUES (3, 6.94, 180.54, 1342, 1);
+INSERT INTO public.properties VALUES (4, 9.0122, 1287, 2470, 1);
+INSERT INTO public.properties VALUES (1, 1.008, -259.1, -252.9, 2);
+INSERT INTO public.properties VALUES (2, 4.0026, -272.2, -269, 2);
+INSERT INTO public.properties VALUES (6, 12.011, 3550, 4027, 2);
+INSERT INTO public.properties VALUES (7, 14.007, -210.1, -195.8, 2);
+INSERT INTO public.properties VALUES (8, 15.999, -218, -183, 2);
+INSERT INTO public.properties VALUES (5, 10.81, 2075, 4000, 3);
+INSERT INTO public.properties VALUES (9, 18.998, -220, -188.1, 2);
+INSERT INTO public.properties VALUES (10, 20.18, -248.6, -246.1, 2);
 
 
 --
